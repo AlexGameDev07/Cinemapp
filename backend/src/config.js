b06523faa224860584dce52c37055fd40a3b7c58
AlexDev07 <alekeymurcia@gmail.com>
2025-05-13 08:06:04 -0600

    Commit inicial, creación repo y configuración librerías

---
+++ backend/src/config.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const config = {
+    db: {
+        DB_URI: process.env.DB_URI
+    },
+    server:{
+        PORT: process.env.PORT
+    },
+    jwt:{
+        JWT_SECRET: process.env.JWT_SECRET,
+        JWT_EXPIRES: process.env.JWT_EXPIRES,
+    },
+    emailSender: {
+        SENDER_EMAIL: process.env.SENDER_EMAIL,
+        SENDER_PASSWORD: process.env.SENDER_PASSWORD,
+    },
+    cloudinary: {
+        CLOUDINARY_NAME: process.env.CLOUDINARY_NAME,
+        CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+        CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+        CLOUDINARY_URL: process.env.CLOUDINARY_URL,
+    }
+}
