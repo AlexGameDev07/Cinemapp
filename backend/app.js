0f6e13f4be85fe761a0e5181fd6a3a938873b14a
AlexDev07 <alekeymurcia@gmail.com>
2025-05-13 09:58:50 -0600

    Add movies routes to the main application file

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 //Import routes
 import customersRoutes from "./src/routes/customersRoutes.js"
 import emloyeesRoutes from "./src/routes/employeesRoutes.js"
+import moviesRoutes from "./src/routes/moviesRoutes.js"
 
 
 //settings
@@ -14,5 +15,6 @@ app.use(cookieParser());
 //Endpoints
 app.use("/api/customers", customersRoutes);
 app.use("/api/employees", emloyeesRoutes);
+app.use("/api/movies", moviesRoutes);
 
 export default app;
--- backend/src/routes/moviesRoutes.js
+++ backend/src/routes/moviesRoutes.js
@@ -4,3 +4,5 @@ import movieCtrl from "../controllers/moviesCtrl.js";
 const router = e.Router();
 router.route('/').get(movieCtrl.getMovies).post(movieCtrl.createMovie);
 router.route('/:id').put(movieCtrl.updateMovie).delete(movieCtrl.deleteMovie);
+
+export default router;
